152d78bbd6582f85fd851bbfd2c4f40cb348bc66
reichg <gabe.reichenberger@gmail.com>
2017-06-20 17:14:59 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if len(request.form['username']) < 3 or len(request.form['password']) < 3 or len(request.form['username']) > 20 or len(request.form['password']) > 20 or (" " in request.form['username']) or (" " in request.form['password']):
-            return render_template('index.html') 
-        else:
-            return successful_login()
-        
-        
-        
-    return render_template('index.html')
+        return render_template('welcome.html')
+         
+    else:
+        return render_template('index.html') 
 
 
 @app.route('/welcome')
